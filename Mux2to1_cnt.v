f81b00ae779b7cff795d5f7e12077398d68b262d
feicaixian <1781349650@qq.com>
2018-05-27 11:58:38 +0800

    Update Mux2to1_cnt.v

--- Mux2to1_cnt.v
+++ Mux2to1_cnt.v
@@ -1,7 +1,7 @@
 module Mux2to1_cnt(Count_CP, Cnt_CP, CPx, measure_mode);
 output reg 	Count_CP;
 input 		Cnt_CP, CPx, measure_mode;
-always @(measure_mode) //measure_mode or Cnt_CP or CPx | measure_mode
+always @(measure_mode or Cnt_CP or CPx) //measure_mode or Cnt_CP or CPx | measure_mode
 begin
 	case(measure_mode)
 		0:Count_CP <= CPx;
